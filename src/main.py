cc8dd7c1be55c046cbd8dd73b3a0c8532897142f
danthoxy <danthoxy@gmail.com>
2021-09-03 12:02:56 +0200

    Mise a jour de la fonction python

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 def main():
-    texte = input("Veuillez entrer la chaine de charactères à modifier")
-    return texte.upper
+    texte = input("Veuillez entrer la chaine de charactères à modifier : \n")
+    print(texte.upper())
+    return
 
 if __name__ == "__main__":
     main()
